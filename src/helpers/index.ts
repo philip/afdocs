038204680cf566a9a362c95deff1b4355b377e6b
dacharyc <dc@dacharycarey.com>
2026-04-05 20:34:55 -0400

    Support using a config with the CLI

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
-export { loadConfig } from './config.js';
+export { loadConfig, findConfig } from './config.js';
 export { describeAgentDocs, describeAgentDocsPerCheck } from './vitest-runner.js';
 export { looksLikeMarkdown, looksLikeHtml } from './detect-markdown.js';
 export {
